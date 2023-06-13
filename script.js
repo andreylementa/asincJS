0a3415cce1683f1568a397ec8a6b524b01e7080c
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-13 15:32:27 +0300

    Задание 1

--- script.js
+++ script.js
@@ -109,3 +109,36 @@ const getCountryData = function (countryName) {
 };
 
 getCountryData('japan');
+
+// Задание 1
+
+const displayCountryByGPS = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => {
+      if (!response.ok)
+        throw new Error(
+          `Проблема с геокодированием (ошибка ${response.status})`
+        );
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return getDataAndConvertToJSON(
+        `https://restcountries.com/v3.1/name/${data.country.toLowerCase()}`,
+        'Страна не найдена.'
+      );
+    })
+    .then(data => displayCountry(data[0]))
+    .catch(e => {
+      console.error(`${e} 🧐`);
+      displayError(`Что-то пошло не так 🧐: ${e.message} Попробуйте ещё раз!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    })
+
+    .catch(e => console.error(`${e.message} 🧐`));
+};
+
+displayCountryByGPS(35.756, 139.256);

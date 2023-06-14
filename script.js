17026db0f3699f7c4b4d9b0678305880e1ad4ebe
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-14 14:29:56 +0300

    Создание простого promise

--- script.js
+++ script.js
@@ -145,11 +145,77 @@ getCountryData('japan');
 
 // Пример работы с циклом событий
 
-console.log('Начало теста');
-setTimeout(() => console.log('Таймер 0 секунд'), 0);
-Promise.resolve('Выполненное promise 1').then(result => console.log(result));
-Promise.resolve('Выполненное promise 2').then(result => {
-  for (let i = 0; i < 10000000000; i++) {}
-  console.log(result);
+//console.log('Начало теста');
+//setTimeout(() => console.log('Таймер 0 секунд'), 0);
+//Promise.resolve('Выполненное promise 1').then(result => console.log(result));
+//Promise.resolve('Выполненное promise 2').then(result => {
+//  for (let i = 0; i < 10000000000; i++) {}
+//  console.log(result);
+//});
+//console.log('Конец теста');
+
+///////////////////////////////////////////////
+// Создание Простого Promise
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Происходит розыгрыш лотереи 🪄');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('Вы ВЫИГРАЛИ! 💶');
+    } else {
+      reject(new Error('Вы ПРОИГРАЛИ! 😿'));
+    }
+  }, 3000);
 });
-console.log('Конец теста');
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying (промисификация) функции setTimeout()
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(3)
+  .then(() => {
+    console.log('Длительность ожидания 3 секунды');
+    return wait(2);
+  })
+  .then(() => {
+    console.log('Длительность ожидания 2 секунды');
+  });
+
+//setTimeout(() => {
+//  console.log('Прошла 1 секунда');
+//  setTimeout(() => {
+//    console.log('Прошло 2 секунды');
+//    setTimeout(() => {
+//      console.log('Прошло 3 секунды');
+//      setTimeout(() => {
+//        console.log('Прошло 4 секунды');
+//      }, 1000);
+//    }, 1000);
+//  }, 1000);
+//}, 1000);
+
+wait(1)
+  .then(() => {
+    console.log('Прошла 1 секунда');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('Прошла 2 секунда');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('Прошла 3 секунда');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('Прошла 4 секунда');
+    return wait(1);
+  });
+
+Promise.resolve('Resolved!').then(res => console.log(res));
+Promise.reject(new Error('Rejected!')).catch(e => console.error(e));

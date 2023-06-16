271368f771f0cb1ebae93388f1cf8c3c8c31728e
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-16 10:50:14 +0300

    задание 2

--- script.js
+++ script.js
@@ -1,114 +1,114 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+//const btn = document.querySelector('.btn-country');
+//const countriesContainer = document.querySelector('.countries');
 
-//////////////////////////////////////////////////////
-btn.addEventListener('click', () => getCountryData('russia'));
+////////////////////////////////////////////////////////
+//btn.addEventListener('click', () => getCountryData('russia'));
 
-const displayCountry = function (data, className = '') {
-  const currencies = Object.values(data.currencies)[0].name;
-  console.log(currencies);
-  const html = `
-        <article class="country ${className}">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👨‍👩‍👧‍👦</span>${Intl.NumberFormat(
-              'ru'
-            ).format(data.population)} миллионов</p>
-            <p class="country__row"><span>🗣️</span>${
-              Object.values(data.languages)[0]
-            }</p>
-            <p class="country__row"><span>💰</span>${currencies}</p>
-          </div>
-        </article>
-  `;
-  countriesContainer.style.opacity = '1';
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-};
+//const displayCountry = function (data, className = '') {
+//  const currencies = Object.values(data.currencies)[0].name;
+//  console.log(currencies);
+//  const html = `
+//        <article class="country ${className}">
+//          <img class="country__img" src="${data.flags.png}" />
+//          <div class="country__data">
+//            <h3 class="country__name">${data.name.common}</h3>
+//            <h4 class="country__region">${data.region}</h4>
+//            <p class="country__row"><span>👨‍👩‍👧‍👦</span>${Intl.NumberFormat(
+//              'ru'
+//            ).format(data.population)} миллионов</p>
+//            <p class="country__row"><span>🗣️</span>${
+//              Object.values(data.languages)[0]
+//            }</p>
+//            <p class="country__row"><span>💰</span>${currencies}</p>
+//          </div>
+//        </article>
+//  `;
+//  countriesContainer.style.opacity = '1';
+//  countriesContainer.insertAdjacentHTML('beforeend', html);
+//};
 
-const getCountryAndBorderCountries = function (country) {
-  // Вызов AJAX для получения данных о стране
-  const request1 = new XMLHttpRequest();
-  request1.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request1.send();
+//const getCountryAndBorderCountries = function (country) {
+//  // Вызов AJAX для получения данных о стране
+//  const request1 = new XMLHttpRequest();
+//  request1.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//  request1.send();
 
-  request1.addEventListener('load', function () {
-    const [data1] = JSON.parse(this.responseText);
-    console.log(data1);
+//  request1.addEventListener('load', function () {
+//    const [data1] = JSON.parse(this.responseText);
+//    console.log(data1);
 
-    // Отображение страны
-    displayCountry(data1);
-    // получаем первую соседнюю страну
-    const [firstNeighbour] = data1.borders;
-    if (!firstNeighbour) return;
-    // Вызов AJAX для получения данных о первой соседней стране
-    const request2 = new XMLHttpRequest();
-    request2.open(
-      'GET',
-      `https://restcountries.com/v3.1/alpha/${firstNeighbour}`
-    );
-    request2.send();
+//    // Отображение страны
+//    displayCountry(data1);
+//    // получаем первую соседнюю страну
+//    const [firstNeighbour] = data1.borders;
+//    if (!firstNeighbour) return;
+//    // Вызов AJAX для получения данных о первой соседней стране
+//    const request2 = new XMLHttpRequest();
+//    request2.open(
+//      'GET',
+//      `https://restcountries.com/v3.1/alpha/${firstNeighbour}`
+//    );
+//    request2.send();
 
-    request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
-      displayCountry(data2, 'neighbour');
-    });
-  });
-};
+//    request2.addEventListener('load', function () {
+//      const [data2] = JSON.parse(this.responseText);
+//      displayCountry(data2, 'neighbour');
+//    });
+//  });
+//};
 
-//getCountryAndBorderCountries('usa');
+////getCountryAndBorderCountries('usa');
 
-//const fetchReq = fetch(`https://restcountries.com/v3.1/name/russia`);
-//console.log(fetchReq);
+////const fetchReq = fetch(`https://restcountries.com/v3.1/name/russia`);
+////console.log(fetchReq);
 
-const displayError = function (message) {
-  countriesContainer.insertAdjacentText('beforeend', message);
-  //countriesContainer.style.opacity = 1;
-};
+//const displayError = function (message) {
+//  countriesContainer.insertAdjacentText('beforeend', message);
+//  //countriesContainer.style.opacity = 1;
+//};
 
-const getDataAndConvertToJSON = function (
-  url,
-  errorMessage = 'Что-то пошло не так.'
-) {
-  return fetch(url).then(response => {
-    if (!response.ok) {
-      throw new Error(`${errorMessage} Ошибка ${response.status}`);
-    }
+//const getDataAndConvertToJSON = function (
+//  url,
+//  errorMessage = 'Что-то пошло не так.'
+//) {
+//  return fetch(url).then(response => {
+//    if (!response.ok) {
+//      throw new Error(`${errorMessage} Ошибка ${response.status}`);
+//    }
 
-    return response.json();
-  });
-};
+//    return response.json();
+//  });
+//};
 
-//
+////
 
-const getCountryData = function (countryName) {
-  getDataAndConvertToJSON(
-    `https://restcountries.com/v3.1/name/${countryName}`,
-    'Страна не найдена'
-  )
-    .then(data => {
-      displayCountry(data[0]);
-      if (!data[0].borders) throw new Error('Соседних стран не найдено');
-      const firstNeighbour = data[0].borders[0];
+//const getCountryData = function (countryName) {
+//  getDataAndConvertToJSON(
+//    `https://restcountries.com/v3.1/name/${countryName}`,
+//    'Страна не найдена'
+//  )
+//    .then(data => {
+//      displayCountry(data[0]);
+//      if (!data[0].borders) throw new Error('Соседних стран не найдено');
+//      const firstNeighbour = data[0].borders[0];
 
-      if (!firstNeighbour) return;
-      return getDataAndConvertToJSON(
-        `https://restcountries.com/v3.1/alpha/${firstNeighbour}`,
-        'Страна не найдена'
-      );
-    })
-    .then(data => displayCountry(data[0], 'neighbour'))
-    .catch(e => {
-      console.error(e);
-      displayError(`Что-то пошло не так, ${e.message}`);
-    })
-    .finally(() => (countriesContainer.style.opacity = 1));
-};
+//      if (!firstNeighbour) return;
+//      return getDataAndConvertToJSON(
+//        `https://restcountries.com/v3.1/alpha/${firstNeighbour}`,
+//        'Страна не найдена'
+//      );
+//    })
+//    .then(data => displayCountry(data[0], 'neighbour'))
+//    .catch(e => {
+//      console.error(e);
+//      displayError(`Что-то пошло не так, ${e.message}`);
+//    })
+//    .finally(() => (countriesContainer.style.opacity = 1));
+//};
 
-getCountryData('japan');
+//getCountryData('japan');
 
 // Задание 1
 
@@ -171,11 +171,6 @@ getCountryData('japan');
 //lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
 //// Promisifying (промисификация) функции setTimeout()
-//const wait = function (seconds) {
-//  return new Promise(function (resolve) {
-//    setTimeout(resolve, seconds * 1000);
-//  });
-//};
 
 //wait(3)
 //  .then(() => {
@@ -220,15 +215,63 @@ getCountryData('japan');
 //Promise.resolve('Resolved!').then(res => console.log(res));
 //Promise.reject(new Error('Rejected!')).catch(e => console.error(e));
 
-const getUserPosition = function () {
-  return new Promise(function (resolve, reject) {
-    //navigator.geolocation.getCurrentPosition(
-    // position => resolve(position),
-    //  e => reject(e)
-    //);
-    navigator.geolocation.getCurrentPosition(resolve, reject);
+//const getUserPosition = function () {
+//  return new Promise(function (resolve, reject) {
+//    //navigator.geolocation.getCurrentPosition(
+//    // position => resolve(position),
+//    //  e => reject(e)
+//    //);
+//    navigator.geolocation.getCurrentPosition(resolve, reject);
+//  });
+//};
+//getUserPosition()
+//  .then(res => console.log(res))
+//  .catch(err => console.error(err));
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
   });
 };
-getUserPosition()
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+const imageContainer = document.querySelector('.images');
+
+const createImageElement = function (imagePath) {
+  return new Promise(function (resolve, reject) {
+    const imgEl = document.createElement('img');
+    imgEl.src = imagePath;
+    imgEl.addEventListener('load', function () {
+      imageContainer.append(imgEl);
+      resolve(imgEl);
+    });
+    imgEl.addEventListener('error', function () {
+      reject(new Error('Изображение не найдено'));
+    });
+  });
+};
+
+let currentImage;
+
+createImageElement('./img/image1.jpg')
+  .then(img => {
+    console.log('Изображение 1 загружено');
+    currentImage = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImageElement('./img/image2.jpg');
+  })
+  .then(img => {
+    console.log('Изображение 2 загружено');
+    currentImage = img;
+    return wait(2);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImageElement('./img/image3.jpg');
+  })
+  .then(img => {
+    console.log('Изображение 3 загружено');
+    currentImage = img;
+    return wait(2);
+  })
+  .catch(e => console.error(e));
